4327842e01663629bad92c8d24f3f7096e575c6b
satyam034 <satyamb954@gmail.com>
2021-05-09 17:46:18 +0530

    'changes_done'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import GetMatches from './component/GetMatches';
+import PlayerSearch from './component/PlayerSearch';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={GetMatches} />
+          <Route exact path="/playersearch" component={PlayerSearch} />
         </Switch>
       </Router>
     </div>
